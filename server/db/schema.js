7948523adc51b27aacb562b6e62de41434144d5f
Samuel Gasser <sgasser94@gmail.com>
2021-05-03 22:56:35 -0700

    Changes to NoSQL Schema design

--- server/db/schema.js
+++ server/db/schema.js
@@ -1,21 +1,22 @@
 MONGOOSE SCHEMAS - ONE COLLECTION, TWO SCHEMAS, TWO DOCUMENTS PER PRODUCTID
 
-META SCHEMA - ONE PER PRODUCT ID
+ONE:MANY - PRODUCT:REVIEWS
+
+PRODUCT SCHEMA - ONE PER PRODUCT ID - METADATA & REFERENCE TO UNIQUE REVIEW IDS
 {
-  product_id: Number,
-  meta: Boolean,     // how I differentiate between meta and not meta in query? - should be fast?
-  ratings: {
+  product_id: Number,  // unique identifier
+  ratings: {      // must be incremented each time review written for this product
     1: Number,
     2: Number,
     3: Number,
     4: Number,
     5: Number,
   },
-  recommended: {
+  recommended: {  // must be incremented each time review written for this product
     0: Number,
     1: Number,
   },
-  characteristics: {
+  characteristics: {     // averages need to be calculatd each timea review written
     Size_idnum: String,
     Length_idnum: String,
     Width: String,
@@ -23,61 +24,26 @@ META SCHEMA - ONE PER PRODUCT ID
     Quality: String,
     Fit: String,
   }
-}
-
-
-REVIEWS SCHEMA - ONE PER PRODUCT ID
-{
-  product_id: Number,
-  meta: Boolean,          // differentiate between meta and review - object property lookup so fast?
-  page: Number,
-  count: Number,
-  results: [
+  reviews: [          // must be pushed to each time a review written for this product
+    review_id: Number, // each a unique identifier
     review_id: Number,
-    rating: Number,
-    summary: String,
-    Recommend: Boolean,
-    response: String,
-    body: String,
-    date: Date,
-    reviewer_name: String,
-    helpfulness: Number,
-    reported: Boolean,
-    photos: [
-      { id: Number, url: String },
-    ]
+    etc...
   ]
 }
 
-
-
-
-// Get to /reviews using product_id, count, sort, page
-// product_id
-// page
-// count
-// results - array
-//   review id
-//   rating
-//   summary
-//   recommend
-//   resposne
-//   body
-//   date
-//   reviewer_name
-//   helpfulness
-//   photos - array of objects - id, url
-
-
-// get to /meta using product_id
-// product_id
-//   ratings - object 1-5 with numbers of each review
-//   recommended - object iwth 0, 1
-//   characteristics - object with 6 possible keys
-//     nested - Size - object with id, value
-
-// PUT to /reviews/review_id/helpful
-// increment helpful of that review_id
-
-// PUT to /reviews/review_id/report
-// turn reported boolean to true?
+REVIEW SCHEMA - ONE PER REVIEW
+{
+  review_id: Number, // unique identifier
+  rating: Number,
+  summary: String,
+  Recommend: Boolean,
+  response: String,
+  body: String,
+  date: Date,
+  reviewer_name: String,
+  helpfulness: Number,
+  reported: Boolean,
+  photos: [
+    { id: Number, url: String },
+  ]
+}
