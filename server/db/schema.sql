2d8690618440ebd7dc304893d169c647357df412
Samuel Gasser <sgasser94@gmail.com>
2021-05-05 15:26:52 -0700

    Final schema design.

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -3,70 +3,66 @@ CREATE DATABASE reviewAPI;
 
 USE reviewAPI;
 
+DROP TABLE IF EXISTS products;
+
 CREATE TABLE products (
-  id INTEGER NOT NULL PRIMARY KEY
-  rec_true INTEGER,
-  rec_false INTEGER,
-  rate_1 INTEGER,
-  rate_2 INTEGER,
-  rate_3 INTEGER,
-  rate_4 INTEGER,
-  rate_5 INTEGER,
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
 );
 
+DROP TABLE IF EXISTS reviews;
+
 CREATE TABLE reviews (
-  review_id INTEGER NOT NULL PRIMARY KEY,
-  rating INTEGER,
-  summary VARCHAR(60),
-  recommended BOOLEAN,
-  response VARCHAR(1000),
-  body VARCHAR(1000),
-  date TIMESTAMPTZ DEFAULT NOW(),
-  reviewer_name VARCHAR(30),
-  helpfulness INTEGER,
-  reported BOOLEAN,
+  id INTEGER NOT NULL PRIMARY KEY,
   product_id INTEGER NOT NULL,
+  rating INTEGER,
+  date TIMESTAM DEFAULT NOW(),
+  summary VARCHAR(60),
+  body TEXT,
+  recommended BOOLEAN,
+  reported BOOLEAN,
+  reviewer_name VARCHAR(20),
+  reviewer_email VARCHAR(30),
+  response TEXT,
+  helpfulness INTEGER,
+
   CONSTRAINT fk_reviews_products
     FOREIGN KEY(product_id)
       REFERENCES products(id)
 );
 
+DROP TABLE IF EXISTS photos;
+
 CREATE TABLE photos (
-  photo_id INTEGER NOT NULL PRIMARY KEY,
-  url VARCHAR(100) NOT NULL,
+  id INTEGER NOT NULL PRIMARY KEY,
+  url VARCHAR(100),
   review_id INTEGER NOT NULL,
   CONSTRAINT fk_photos
     FOREIGN KEY(review_id)
-      REFERENCES reviews(review_id)
+      REFERENCES reviews(id)
 );
+
+DROP TABLE IF EXISTS characteristics;
 
 CREATE TABLE characteristics (
   id INTEGER NOT NULL PRIMARY KEY,
+  product_id INTEGER,
   name VARCHAR(20),
+  CONSTRAINT fk_characteristics
+    FOREIGN KEY (product_id)
+      REFERENCES products(id)
 );
 
-CREATE TABLE reviews_chars (
-  id INTEGER NOT NULL PRIMARY KEY,
-  review_id INTEGER NOT NULL,
+DROP TABLE IF EXISTS reviews_characteristics;
+
+CREATE TABLE reviews_characteristics (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
   characteristic_id INTEGER NOT NULL,
+  review_id INTEGER NOT NULL,
   value INTEGER NOT NULL,
   CONSTRAINT fk_reviews_chars
     FOREIGN KEY(review_id)
-      REFERENCES reviews(review_id)
+      REFERENCES reviews(review_id),
   CONSTRAINT fk_chars_reviews
     FOREIGN KEY(characteristic_id)
       REFERENCES characteristics(id)
-);
-
-CREATE TABLE product_chars (
-  id INTEGER NOT NULL PRIMARY KEY,
-  product_id INTEGER NOT NULL,
-  characteristic_id INTEGER NOT NULL,
-  average VARCHAR(6),
-  CONSTRAINT fk_chars_products
-    FOREIGN KEY(characteristic_id)
-      REFERENCES characteristics(id)
-  CONSTRAINT fk_products_chars
-    FOREIGN KEY(product_id)
-      REFERENCES products(id)
 );
