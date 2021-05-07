606a8f827c8d86922680abc50ed9862e3ac4d9ff
Samuel Gasser <sgasser94@gmail.com>
2021-05-06 22:23:04 -0700

    Cleaned csv's rewrote them into new files that can be loaded directly into db.

--- parsers/parseReviews.js
+++ parsers/parseReviews.js
@@ -1,21 +1,38 @@
-const csv = require('csv-parser');
 const fs = require('fs');
+const readline = require('readline');
 
-const reviewsResults = [];
 
-fs.createReadStream('./csv/reviews_short.csv')
-  .pipe(csv({
-    headers: ['id', 'product_id', 'rating', 'date', 'summary', 'body', 'recommended', 'reported', 'reviewer_name', 'reviewer_email', 'response', 'helpfulness'],
-    skipLines: 1,
-  }))
-  .on('data', (data) => {
-    // data.date = new SimpleDateFormat(data.date);
-    // data.date = sdf.parse(data.date);
-    var submissionDate = new Date(parseInt(data.date));
-    data.date = submissionDate.toUTCString();
-    reviewsResults.push(data);
+async function cleanReviews() {
+  var source = './csv/reviews.csv';
+  var destination = './csv/clean_Reviews.csv';
+  const inStream = fs.createReadStream(source);
+  const outStream = fs.createWriteStream(destination);
+
+  outStream.write(`id,product_id,rating,date,summary,body,recommend,reported,reviewer_name,reviewer_email,response,helpfulness\n`);
+
+  const rl = readline.createInterface({
+    input: inStream,
+    output: outStream,
+    CRLF: Infinity
   })
-  .on('error', (err) => console.log(err))
-  .on('end', () => console.log('reviews', reviewsResults));
 
-// CHECK THIS OUT
+  rl.on('line', (line) => {
+    var row = line.split(',');
+    if (row[0] === 'id' || Number(row[0]) < 1) return;
+    if (Number(row[1]) < 1) return;
+    if (Number(row[2]) < 1 || Number(row[2]) > 5) return;
+    if (!parseInt(row[3])) return;
+    if (row[4].length > 60 || row[4] === '') return;
+    if (row[5].length > 1000 || row[5].length < 50) return;
+    if (row[6] !== 'true' && row[6] !== 'false') return;
+    if (row[7] !== 'true' && row[7] !== 'false') return;
+    if (row[8].length > 30 || row[8] === '') return;
+    if (row[9].length > 40 || row[9] === '') return;
+    if (typeof row[10] !== 'string') return;
+    if (Number(row[11]) < 0) return;
+
+    outStream.write(`${row[0]},${row[1]},${row[2]},${parseInt(row[3])},${row[4]},${row[5]},${row[6]},${row[7]},${row[8]},${row[9]},${row[10]},${row[11]}\n`);
+  })
+}
+
+cleanReviews();
