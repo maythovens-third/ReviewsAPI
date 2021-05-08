1ec04a20f6dac6ba6399d0dc049ae4f32125d0d3
Samuel Gasser <sgasser94@gmail.com>
2021-05-08 10:49:23 -0700

    Changed method names in routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,11 @@
 const controller = require('./controller');
 var router = require('express').Router();
 
-router.get('/reviews/', controller.get);
+router.get('/reviews/', controller.list);
 
 router.get('/reviews/meta/', controller.meta);
 
-router.post('/reviews', controller.post);
+router.post('/reviews', controller.create);
 
 router.put('/reviews/:review_id/helpful', controller.helpful);
 
