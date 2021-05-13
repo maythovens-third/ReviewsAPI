edb33f9c123870315c36fbc86d3188c7426f7654
Ubuntu <ubuntu@ip-172-31-21-8.us-west-2.compute.internal>
2021-05-13 23:10:24 +0000

    Loader verificationwq

--- server/controller/sendToken.js
+++ server/controller/sendToken.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
   sendToken: (req, res) => {
-    res.sendFile(path.join(__dirname, '../loaderio-965daee38be22f32824976e869fc84e1.txt'));
-    res.end();
+    res.sendFile(__dirname +  '/loaderio-965daee38be22f32824976e869fc84e1.txt');
   }
 }
