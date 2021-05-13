0beab2e020826adaf031ea3f0e7cb8e934055830
Samuel Gasser <sgasser94@gmail.com>
2021-05-13 15:19:49 -0700

    Parentehses

--- server/controller/sendToken.js
+++ server/controller/sendToken.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   sendToken: (req, res) => {
-    res.sendFile(path.join(__dirname, '../loaderio-965daee38be22f32824976e869fc84e1.txt');
+    res.sendFile(path.join(__dirname, '../loaderio-965daee38be22f32824976e869fc84e1.txt'));
     res.end();
   }
 }
