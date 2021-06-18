e6e2f5d17c07986426935c9ab2675f6db9d672e4
Samuel Gasser <sgasser94@gmail.com>
2021-06-18 15:24:23 -0700

    Working on readme.

--- test/performance/k6script.js
+++ test/performance/k6script.js
@@ -5,13 +5,12 @@ export let options = {
     { duration: '30s', target: 200 },
     { duration: '10s', target: 700 },
     { duration: '10s', target: 1500 },
-    { duration: '2m20s', target: 3000 },
+    { duration: '1m30s', target: 3000 },
     { duration: '10s', target: 1500 },
     { duration: '20s', target: 200 },
   ],
 };
 export default function () {
-  // http.post(url, data, params);
   http.get('http://localhost:3000/reviews/meta?product_id=23180');
   sleep(1);
 }
